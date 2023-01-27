afd35ddeffc1bf9700f141e0f874490f9f7130a1
manokrish2000 <manokrish104525@gmail.com>
2023-01-27 10:02:28 +0530

    API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from "react"
+// import axios from "axios"
+const API = "https://new-folder-ruddy.vercel.app/"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+      <PhoneList />
     </div>
   );
+}
+function PhoneList() {
+  const [mobiles, setMobiles] = useState([])
+
+  useEffect(() => {
+
+    fetch(`${API}/mobiles1`)
+      .then((data) => data.json())
+      .then((mbs) => setMobiles(mbs))
+    // async function Mobilelist() {
+    //   let mobile = await window.fetch('http://localhost:4000/mobiles')
+    //   let data = await mobile.json()
+
+    //   console.log(data)
+    // } Mobilelist()
+  }, [])
+  return <div className="phone-list-container">
+    {mobiles.map((mb) => (<Phone mobile={mb} />))}
+  </div>
+}
+function Phone({ mobile }) {
+  // const mobile = {
+
+  //   model: "OnePlus 9 5G",
+  //   img: "https://m.media-amazon.com/images/I/61fy+u9uqPL._SX679_.jpg",
+  //   company: "Oneplus"
+
+  // }
+  return (
+    <div className="phone-container">
+      <img className="phone-picture" src={mobile.img} alt={mobile.model}></img>
+      <h2 className="phone-name">{mobile.model}</h2>
+      <p className="phone-company">{mobile.company}</p>
+
+    </div>
+  )
 }
 
 export default App;
